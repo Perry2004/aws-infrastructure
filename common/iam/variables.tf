5a8b554830b490047772deaa701738dd5fe5038f
Perry Zhu <perryzhu2004@outlook.com>
2025-10-05 12:08:13 -0700

    chore: comment out iam codes since the workspace is not created

--- common/iam/variables.tf
+++ common/iam/variables.tf
@@ -1,18 +1,18 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  default     = "us-west-2"
-}
+# variable "aws_region" {
+#   description = "The AWS region to deploy resources in"
+#   type        = string
+#   default     = "us-west-2"
+# }
 
-variable "iam_admin_users" {
-  description = "List of IAM Identity Center admin users to create"
-  type = list(object({
-    username     = string
-    email        = string
-    first_name   = string
-    last_name    = string
-    display_name = string
-  }))
-  default   = []
-  sensitive = true
-}
+# variable "iam_admin_users" {
+#   description = "List of IAM Identity Center admin users to create"
+#   type = list(object({
+#     username     = string
+#     email        = string
+#     first_name   = string
+#     last_name    = string
+#     display_name = string
+#   }))
+#   default   = []
+#   sensitive = true
+# }
