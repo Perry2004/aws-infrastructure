99577f102646f8838c65404079571d8dd0a570bf
Perry Zhu <perryzhu2004@outlook.com>
2025-10-04 23:31:25 -0700

    fix: handle secrets properly

--- admin_users.tf
+++ admin_users.tf
@@ -3,21 +3,21 @@ data "aws_ssoadmin_instances" "this" {}
 data "aws_caller_identity" "current" {}
 
 resource "aws_identitystore_user" "users" {
-  for_each = { for user in var.iam_admin_users : user.username => user }
+  count = length(var.iam_admin_users)
 
   identity_store_id = data.aws_ssoadmin_instances.this.identity_store_ids[0]
 
-  user_name = each.value.username
+  user_name = var.iam_admin_users[count.index].username
 
-  display_name = each.value.display_name
+  display_name = var.iam_admin_users[count.index].display_name
 
   name {
-    given_name  = each.value.first_name
-    family_name = each.value.last_name
+    given_name  = var.iam_admin_users[count.index].first_name
+    family_name = var.iam_admin_users[count.index].last_name
   }
 
   emails {
-    value = each.value.email
+    value = var.iam_admin_users[count.index].email
   }
 }
 
@@ -33,11 +33,11 @@ resource "aws_ssoadmin_managed_policy_attachment" "admin" {
 }
 
 resource "aws_ssoadmin_account_assignment" "admin" {
-  for_each = aws_identitystore_user.users
+  count = length(var.iam_admin_users)
 
   instance_arn       = data.aws_ssoadmin_instances.this.arns[0]
   permission_set_arn = aws_ssoadmin_permission_set.admin.arn
-  principal_id       = each.value.user_id
+  principal_id       = aws_identitystore_user.users[count.index].user_id
   principal_type     = "USER"
   target_id          = data.aws_caller_identity.current.account_id
   target_type        = "AWS_ACCOUNT"
