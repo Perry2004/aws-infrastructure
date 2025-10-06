3e5d1650d463124178001d12c62c3c7897aa1b6c
Perry Zhu <perryzhu2004@outlook.com>
2025-10-05 21:20:36 -0700

    chore: uncomment iam module

--- common/iam/outputs.tf
+++ common/iam/outputs.tf
@@ -1,11 +1,11 @@
-# output "iam_admin_users" {
-#   description = "Details of created IAM Identity Center admin users"
-#   value = { for i in range(length(var.iam_admin_users)) :
-#     var.iam_admin_users[i].username => {
-#       user_id      = aws_identitystore_user.users[i].user_id
-#       display_name = aws_identitystore_user.users[i].display_name
-#       email        = aws_identitystore_user.users[i].emails[0].value
-#     }
-#   }
-#   sensitive = true
-# }
+output "iam_admin_users" {
+  description = "Details of created IAM Identity Center admin users"
+  value = { for i in range(length(var.iam_admin_users)) :
+    var.iam_admin_users[i].username => {
+      user_id      = aws_identitystore_user.users[i].user_id
+      display_name = aws_identitystore_user.users[i].display_name
+      email        = aws_identitystore_user.users[i].emails[0].value
+    }
+  }
+  sensitive = true
+}
