0015ce01d6f8e31dccba1c81a6edfa8bd9c53d02
Perry Zhu <perryzhu2004@outlook.com>
2025-10-07 21:04:46 -0700

    chore: trigger checks

--- common/dns/dns.tf
+++ common/dns/dns.tf
@@ -5,6 +5,7 @@ import {
 
 resource "aws_route53domains_registered_domain" "perryz_net" {
   domain_name = var.domain_name
+
   name_server {
     name = aws_route53_zone.perryz_net_zone.name_servers[0]
   }
