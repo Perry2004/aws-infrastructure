95d0a2385d05fca0a00379b35b5d54e20025402c
Perry Zhu <perryzhu2004@outlook.com>
2025-11-05 19:28:43 -0800

    revert: single hosted zone

--- apps/cloud-chat-app/usw2dev/dns.tf
+++ apps/cloud-chat-app/usw2dev/dns.tf
@@ -1,3 +1,1 @@
-resource "aws_route53_zone" "chat_subdomain_zone" {
-  name = "${var.subdomain_name}.${data.terraform_remote_state.dns.outputs.domain_name}"
-}
+
--- apps/cloud-chat-app/usw2dev/outputs.tf
+++ apps/cloud-chat-app/usw2dev/outputs.tf
@@ -1,4 +1,1 @@
-output "chat_subdomain_zone_id" {
-  description = "The Route 53 Hosted Zone ID for chat.perryz.net"
-  value       = aws_route53_zone.chat_subdomain_zone.zone_id
-}
+
