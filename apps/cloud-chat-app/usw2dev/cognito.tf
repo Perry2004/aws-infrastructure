b5eb9d6eddcb13de277e2f503dff048f3f3537e5
Perry Zhu <perryzhu2004@outlook.com>
2025-12-07 14:51:43 -0800

    infra: cognito ses configuration

--- apps/cloud-chat-app/usw2dev/cognito.tf
+++ apps/cloud-chat-app/usw2dev/cognito.tf
@@ -1,3 +1,9 @@
 resource "aws_cognito_user_pool" "cca" {
   name = "${var.app_short_name}-user-pool"
+
+  email_configuration {
+    from_email_address    = "Cloud-Chat-App <no-reply@${var.subdomain_name}.${data.terraform_remote_state.dns.outputs.domain_name}>"
+    email_sending_account = "DEVELOPER"
+    source_arn            = aws_ses_email_identity.do_not_reply.arn
+  }
 }
