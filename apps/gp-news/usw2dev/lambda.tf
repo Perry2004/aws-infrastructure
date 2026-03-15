7f8eb33d5829160cddf33e8991d1bb97b6696d53
Perry Zhu <perryzhu2004@outlook.com>
2026-03-15 13:04:54 -0700

    infra: trigger gp news lambda at 0 and 15 o'clock utc time

--- apps/gp-news/usw2dev/lambda.tf
+++ apps/gp-news/usw2dev/lambda.tf
@@ -14,3 +14,30 @@ resource "aws_lambda_function" "gp_news_lambda" {
 
   architectures = ["x86_64"]
 }
+
+resource "aws_cloudwatch_event_rule" "gp_news_periodic" {
+  count = length(var.schedule_daily_utc_time)
+
+  name                = "gp-news-scraper-periodic-trigger-${count.index}"
+  description         = "Trigger GP News scraper lambda daily at ${var.schedule_daily_utc_time[count.index]} UTC"
+  schedule_expression = "cron(${split(":", var.schedule_daily_utc_time[count.index])[1]} ${split(":", var.schedule_daily_utc_time[count.index])[0]} * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "gp_news_scraper_lambda" {
+  count = length(var.schedule_daily_utc_time)
+
+  rule      = aws_cloudwatch_event_rule.gp_news_periodic[count.index].name
+  target_id = "GPNewsScraperLambda-${count.index}"
+  arn       = aws_lambda_function.gp_news_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  count = length(var.schedule_daily_utc_time)
+
+  statement_id  = "AllowExecutionFromEventBridge-${count.index}"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.gp_news_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.gp_news_periodic[count.index].arn
+}
+
--- apps/gp-news/usw2dev/terraform.auto.tfvars
+++ apps/gp-news/usw2dev/terraform.auto.tfvars
@@ -1,1 +1,2 @@
 env_name = "usw2dev"
+schedule_daily_utc_time = ["00:00", "15:00"]
